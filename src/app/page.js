3cf4838fda7d2874e04d2fbd1262fa3fdf327be1
Nizares <nizares234@gmail.com>
2026-05-22 15:28:45 +0800

    change hero section, and try some sort but not working, next step is ticket/invoice

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,51 +14,74 @@ export default function Home() {
   const now = new Date();
 
   useEffect(() => {
-    eventService.getAll()
-      .then(res => {
+    eventService
+      .getAll()
+      .then((res) => {
         const data = res.data.data ?? res.data ?? [];
         setEvents(Array.isArray(data) ? data : []);
       })
-      .catch(err => console.error(err))
+      .catch((err) => console.error(err))
       .finally(() => setLoading(false));
   }, []);
 
+  console.log("1. total events:", events.length)
+  console.log("2. now:", now)
+  console.log("3. setelah filter:", events.filter(item => new Date(item.start_date) > now))
+
   return (
     <Container className="flex flex-col gap-y-16">
-
       <div className="flex flex-col flex-1 items-center justify-center p-8">
-        <h1 className="text-5xl font-bold">Samarinda Hash House Harriers</h1>
-        <h2 className="text-3xl font-semibold">On On!</h2>
-        <p className="p-5">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Donec rhoncus est leo, quis dignissim felis ornare quis.
-        </p>
-        <Image
-          src="/assets/images/sh3logo.png"
-          alt="Logo"
-          width={250}
-          height={250}
-          className="w-40 h-40 md:w-64 md:h-64 lg:w-64 lg:h-64 object-cover rounded-lg"
-        />
-        <Link
-          href="/about"
-          className="bg-btn-green-normal text-white px-6 py-2.5 m-5 rounded-full font-medium hover:bg-btn-green-hover active:bg-btn-green-active transition-colors"
-        >
-          Tentang Kami
-        </Link>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex flex-col w-1/2">
+            <h1 className="text-5xl font-bold">
+              Samarinda <span className="text-text-oranges">Hash </span>House Harriers
+            </h1>
+            <h2 className="text-3xl font-semibold">On On!</h2>
+            <p className="py-5">
+              A Drinking Club With a Running Problem,
+              Kami mengadakan lari mingguan di berbagai sudut kota dan alam Kalimantan, dilanjutkan dengan sesi down-down yang penuh keceriaan.
+              </p>
+            <div className="flex flex-row">
+              <Link
+                href="/about"
+                className="bg-btn-green-normal text-white px-6 py-2.5 m-5 rounded-full font-medium hover:bg-btn-green-hover active:bg-btn-green-active transition-colors"
+              >
+                Tentang Kami
+              </Link>
+              <Link
+                href="/events"
+                className="bg-btn-green-normal text-white px-6 py-2.5 m-5 rounded-full font-medium hover:bg-btn-green-hover active:bg-btn-green-active transition-colors"
+              >
+                Events
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-col p-8 w-1/2 items-center">
+            <Image
+              src="/assets/images/sh3logo.png"
+              alt="Logo"
+              width={250}
+              height={250}
+              className="w-40 h-40 md:w-64 md:h-64 lg:w-64 lg:h-64 object-cover rounded-lg"
+            />
+          </div>
+        </div>
       </div>
 
       <Carousel />
 
-      <h2 className="text-5xl font-bold flex justify-center">Event yang Akan Datang</h2>
+      <h2 className="text-5xl font-bold flex justify-center">
+        Event yang Akan Datang
+      </h2>
       <div className="flex md:flex-row justify-center gap-8 flex-col items-center">
         {loading ? (
           <p className="text-xl">Loading...</p>
         ) : (
           events
-            .filter(item => new Date(item.start_date) > now)
+            .filter((item) => new Date(item.start_date) > now)
+            .sort((a, b) => new Date(b.start_date) - new Date(a.start_date))
             .slice(0, 3)
-            .map(item => (
+            .map((item) => (
               <EventCard
                 key={item.id}
                 id={item.id}
@@ -73,16 +96,18 @@ export default function Home() {
         )}
       </div>
 
-      <h2 className="text-5xl font-bold flex justify-center">Event yang Sudah Selesai</h2>
+      <h2 className="text-5xl font-bold flex justify-center">
+        Event yang Sudah Selesai
+      </h2>
       <div className="flex justify-center">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 items-center">
           {loading ? (
             <p className="text-xl">Loading...</p>
           ) : (
             events
-              .filter(item => new Date(item.start_date) < now)
+              .filter((item) => new Date(item.start_date) < now)
               .slice(0, 4)
-              .map(item => (
+              .map((item) => (
                 <EventCard
                   key={item.id}
                   id={item.id}
@@ -97,7 +122,6 @@ export default function Home() {
           )}
         </div>
       </div>
-
     </Container>
   );
 }
